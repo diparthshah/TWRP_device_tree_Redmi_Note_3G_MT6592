f4c98a131d84bfbe2e761c50d94c0208abc3cfdc
DIPARTH SHAH <diparths@gmail.com>
2016-04-08 22:48:39 +0530

    Fixing rest tree

--- device_minote.mk
+++ device_minote.mk
@@ -3,9 +3,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 # The gps config appropriate for this device
 $(call inherit-product, device/common/gps/gps_us_supl.mk)
 
-$(call inherit-product-if-exists, vendor/xiaomi/minote/minote-vendor.mk)
-
-DEVICE_PACKAGE_OVERLAYS += device/xiaomi/minote/overlay
 
 LOCAL_PATH := device/xiaomi/minote
 ifeq ($(TARGET_PREBUILT_KERNEL),)
