6a7d17e15eee2aeeb8038bf310d63f34c596ad4c
EndLess728 <aryanmk.mk5@gmail.com>
2016-04-08 22:14:41 +0530

    updated Boardconfig.mk with more aqurate flags

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -18,8 +18,12 @@ TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv7-a-neon
 ARCH_ARM_HAVE_TLS_REGISTER := true
 
+# Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := minote
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_SPARSE_EXT_DISABLED := false
 
+# Kernel
 BOARD_KERNEL_CMDLINE := 
 BOARD_KERNEL_BASE := 0x10000000
 BOARD_KERNEL_PAGESIZE := 2048
@@ -30,18 +34,43 @@ BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16777216
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 838860800
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 6453985280
 BOARD_FLASH_BLOCK_SIZE := 131072
-TARGET_USERIMAGES_USE_EXT4 := true
+
+#board config 
 BOARD_SUPPRESS_SECURE_ERASE := true
 BOARD_SUPPRESS_EMMC_WIPE := true 
-TARGET_PREBUILT_KERNEL := device/xiaomi/minote/kernel
-
+BOARD_CUSTOM_BOOTIMG := true
 BOARD_HAS_NO_SELECT_BUTTON := true
+BOARD_HAS_MTK := true
+
+TARGET_PREBUILT_KERNEL := device/xiaomi/minote/kernel
+BOARD_CUSTOM_BOOTIMG_MK := device/xiaomi/minote/boot.mk
+
+TARGET_KMODULES := true
 
 #CWM and CTR Recovery
 #TARGET_RECOVERY_INITRC := $(DEVICE_FOLDER)/recovery/recovery.ctr.rc
 TARGET_RECOVERY_FSTAB := $(DEVICE_FOLDER)/recovery.fstab
 
-BOARD_HAS_MTK := true
+# Assert
+TARGET_OTA_ASSERT_DEVICE := minote,htt92_wet_jb9
+
+COMMON_GLOBAL_CFLAGS += -DDISABLE_HW_ID_MATCH_CHECK
+TARGET_RUNNING_WITHOUT_SYNC_FRAMEWORK := true
+
+#EGL
+BOARD_EGL_CFG := $(DEVICE_FOLDER)/egl.cfg
+USE_OPENGL_RENDERER := true
+#BOARD_EGL_WORKAROUND_BUG_10194508 := true
+USE_OPENGL_RENDERER := true
+
+# MTK Hardware
+BOARD_HAS_MTK_HARDWARE := true
+BOARD_USE_MTK_LAYOUT := true
+MTK_HARDWARE := true
+COMMON_GLOBAL_CFLAGS += -DMTK_HARDWARE -DADD_LEGACY_ACQUIRE_BUFFER_SYMBOL
+COMMON_GLOBAL_CPPFLAGS += -DMTK_HARDWARE
+
+
 # MTK Partitions Defines
 MTK_BOOT_DEVICE_NAME := /dev/bootimg
 MTK_BOOT_DEVICE_SIZE := 16777216
@@ -52,25 +81,23 @@ MTK_UBOOT_DEVICE_SIZE := 4194304
 MTK_NVRAM_DEVICE_NAME := /dev/nvram
 MTK_NVRAM_DEVICE_SIZE := 5242880
 BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_15x24.h\"
-DEVICE_RESOLUTION := 720x1280
 #BOARD_UMS_LUNFILE := "/sys/devices/platform/mt_usb/musb-hdrc.0/gadget/lun0/file"
 #TARGET_RECOVERY_LCD_BACKLIGHT_PATH := /sys/devices/platform/leds-mt65xx/leds/lcd-backlight/brightness
 
 #TWRP
 TW_THEME := portrait_hdpi
-#TARGET_RECOVERY_INITRC := $(DEVICE_FOLDER)/recovery/recovery.twrp.rc
-#TARGET_RECOVERY_FSTAB := $(DEVICE_FOLDER)/recovery/recovery.twrp.fstab
+DEVICE_RESOLUTION := 720x1280
+TARGET_SCREEN_WIDTH := 720
+TARGET_SCREEN_HEIGHT := 1280
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
 TW_SDEXT_NO_EXT4 := true
 TW_EXCLUDE_MTP := true
-TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
-TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/class/android_usb/android0/f_mass_storage/lun/file"
-TW_CUSTOM_CPU_TEMP_PATH := "/sys/class/power_supply/battery/batt_temp"
+TW_BRIGHTNESS_PATH := "/sys/devices/platform/leds-mt65xx/leds/lcd-backlight/brightness"
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/mt_usb/musb-hdrc.0/gadget/lun%d/file"
+TW_CUSTOM_CPU_TEMP_PATH := /sys/devices/virtual/thermal/thermal_zone1/temp
 RECOVERY_SDCARD_ON_DATA := true
 BOARD_HAS_LARGE_FILESYSTEM := true
-RECOVERY_GRAPHICS_USE_LINELENGTH := true
 TW_NO_REBOOT_BOOTLOADER := true
-#TW_USE_TOOLBOX := true
-#TW_HAS_DOWNLOAD_MODE := true
 TW_DEFAULT_EXTERNAL_STORAGE := true
 TW_INTERNAL_STORAGE_PATH := "/data/media"
 TW_INTERNAL_STORAGE_MOUNT_POINT := "data"
@@ -85,10 +112,9 @@ SP2_DISPLAY_NAME := "nvram"
 SP2_BACKUP_METHOD := image
 SP2_MOUNTABLE := 0
 TW_INCLUDE_FB2PNG := true
+BOARD_USE_NTFS_3G := false
+TW_EXTRA_LANGUAGES := true
 
-# EGL settings
-BOARD_EGL_CFG := $(DEVICE_FOLDER)/egl.cfg
-USE_OPENGL_RENDERER := true
 # Set insecure for root access and device specifics
 ADDITIONAL_DEFAULT_PROPERTIES += ro.secure=0 \
 ro.allow.mock.location=1 \
@@ -97,14 +123,3 @@ ro.debuggable=1 \
 persist.service.acm.enable=1 \
 persist.sys.usb.config=mass_storage,adb \
 ro.mount.fs=EXT4
-
-#PhilZ Touch build flags for MTK
-TARGET_COMMON_NAME := minote
-BOARD_USE_MTK_LAYOUT := true
-BOARD_USE_NTFS_3G := false
-TARGET_SCREEN_WIDTH := 720
-TARGET_SCREEN_HEIGHT := 1280
-TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/class/android_usb/android0/f_mass_storage/lun/file
-TARGET_DEVICE := minote
-BRIGHTNESS_SYS_FILE := /sys/class/leds/lcd-backlight/brightness
-BOARD_CUSTOM_BOOTIMG_MK := device/xiaomi/minote/boot.mk
